1c2d0072112499c1038742d7fb053dfa611de005
Noumankhan <nk2752097@gmail.com>
2025-11-22 20:03:27 +0530

    abc

---
+++ lords_college/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lords_college"
---
+++ lords_college1/src/main/java/org/example/data_type/NonPrimitiveDataType.java
@@ -0,0 +1,8 @@
+package org.example.data_type;
+
+public class NonPrimitiveDataType {
+    public static void main(String[] args) {
+        String name = "Numan";
+        System.out.println("Name is: " + name);
+    }
+}
